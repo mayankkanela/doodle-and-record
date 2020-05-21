c1d983c81637d0ba1f372b27ee904d528c4f73c7
mayankkanela <mayankkanela8@gmail.com>
2020-05-21 19:53:00 +0530

    firebase upload added

--- app/src/main/java/com/mayank/doodleandrecord/UploadAndPreviewActivity.java
+++ app/src/main/java/com/mayank/doodleandrecord/UploadAndPreviewActivity.java
@@ -1,9 +1,11 @@
 package com.mayank.doodleandrecord;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +13,18 @@ import android.widget.EditText;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UploadAndPreviewActivity extends AppCompatActivity {
     //views
@@ -58,7 +72,7 @@ public class UploadAndPreviewActivity extends AppCompatActivity {
                 if (!etTitle.getText().toString().isEmpty()) {
                     startUpload();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Please Enter title", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(UploadAndPreviewActivity.this, "Please Enter title", Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -66,10 +80,48 @@ public class UploadAndPreviewActivity extends AppCompatActivity {
     }
 
     private void startUpload() {
-        ProgressDialog progressDialog = new ProgressDialog(getApplicationContext());
+        final ProgressDialog progressDialog = new ProgressDialog(UploadAndPreviewActivity.this);
         progressDialog.setMessage("Uploading...");
         progressDialog.setCancelable(false);
         progressDialog.show();
+        caption = etTitle.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, String> item = new HashMap<>();
+        item.put("caption", caption);
+        item.put("videoTitle", name);
+        db.collection("record").add(item).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                progressDialog.setMessage("Uploading Video...");
+                uploadVideo(progressDialog, documentReference);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(UploadAndPreviewActivity.this, "Failed to upload!", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        });
+    }
 
+    private void uploadVideo(final ProgressDialog progressDialog, DocumentReference documentReference) {
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReference();
+        StorageReference videoReference = storageReference.child("videos/" + documentReference.getId());
+        UploadTask uploadTask = videoReference.putFile(Uri.fromFile(new File(path)));
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(UploadAndPreviewActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+                UploadAndPreviewActivity.this.finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(UploadAndPreviewActivity.this, "Failed to upload video!", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        });
     }
 }
